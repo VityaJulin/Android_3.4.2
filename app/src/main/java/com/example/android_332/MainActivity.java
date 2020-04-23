24c22ad5427d502e3a36dcf7f45485528cae7179
vityajulin <vkdvornokov@gmail.com>
2020-04-23 20:05:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android_332/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.android_332;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private Spinner langSpin;
+    private Button okBtn;
+    private TextView primaryTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.lang_spin, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        langSpin.setAdapter(adapter);
+
+        langSpin.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String[] languages = getResources().getStringArray(R.array.lang_spin);
+                Toast.makeText(MainActivity.this, R.string.toast_select, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (langSpin.getSelectedItemPosition() == 0) {
+                    Locale locale = new Locale("ru");
+                    Configuration config = new Configuration();
+                    config.setLocale(locale);
+                    getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                    recreate();
+                } else if (langSpin.getSelectedItemPosition() == 1) {
+                    Locale locale = new Locale("en", "us");
+                    Configuration config = new Configuration();
+                    config.setLocale(locale);
+                    getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                    recreate();
+                }
+            }
+        });
+
+    }
+
+    private void initViews() {
+        langSpin = (Spinner) findViewById(R.id.land_spin);
+        primaryTxt = findViewById(R.id.primary_txt);
+        okBtn = findViewById(R.id.ok_btn);
+    }
+}
